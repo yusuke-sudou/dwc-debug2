8c376f61f8b9e199866087b94bc8aa92daa48381
yusuke-sudou <sudoukky22@gmail.com>
2024-02-04 14:36:35 +0900

    [Add]投稿一覧修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
