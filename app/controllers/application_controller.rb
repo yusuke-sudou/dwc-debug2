71bbfcc0bae63c803b7b4e45e355c4044f317611
yusuke-sudou <sudoukky22@gmail.com>
2024-02-07 14:17:39 +0900

    [Add]Rspec2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+  
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
