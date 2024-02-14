4c1acaa947aa522697c384932918023634d198c0
yusuke-sudou <sudoukky22@gmail.com>
2024-02-14 14:03:36 +0900

    [Add]Book投稿のバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,12 +13,14 @@ class BooksController < ApplicationController
   end
 
   def create
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      flash[:notice] = "投稿に失敗しました"
       render 'index'
     end
   end
